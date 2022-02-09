2daa54c5ac58334393ca8ec9cca465a6c4736566
mihai <mindrescumihai99@gmail.com>
2022-02-09 15:54:40 +0200

    created react app

---
+++ lab2/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Mindrescu Mihai
+      </h1>
+      <h2>RM-191</h2>
+    </div>
+  );
+}
+
+export default App;
